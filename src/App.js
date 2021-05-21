0273d648714da305b2c997a381f35e218e404200
Yuki <yukipooh4120@gmail.com>
2021-05-20 20:08:42 -0700

    Form display control added

--- src/App.js
+++ src/App.js
@@ -4,23 +4,54 @@ import Form from "./component/Form/Form";
 //import function
 
 class App extends Component {
+  state = {
+    formDisplay: "block" //default none
+  };
+
   //fetch API (Yuri)
 
-  //delete function (Yuri)
+  //delete method (Yuri)
 
-  //edit function (Yuki)
-  //display none --> blockにするfunction
+  /* **************** Form.js control **************** */
+  //edit method (Yuki)
+  editTitle = () => {
+    //show modal
+    this.setState({
+      formDisplay: "block"
+    });
+  };
+
+  //close button clicked
+  closeForm = () => {
+    //hide modal
+    this.setState({
+      formDisplay: "none"
+    });
+  };
+
+  //save change method
+  saveChange = (e) => {
+    console.log("save clicked");
+    e.preventDefault();
+    //hide modal
+    this.setState({
+      formDisplay: "none"
+    });
+  };
 
   render() {
     return (
       <>
         <header className="App-header">
-          <h1>Weekly Assignment</h1>
+          <h1>React Photo Gallery</h1>
         </header>
         {/* Photo.js */}
 
         {/* Form.js */}
-        <Form />
+        <Form
+          formDisplay={this.state.formDisplay}
+          saveChange={this.saveChange}
+          closeForm={this.closeForm} />
       </>
     );
   }
