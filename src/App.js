f05ba9260c8bec5de4cf0cf39c76c45f26cc2da0
yurihashim <yurihashim@icloud.com>
2021-05-20 17:50:26 -0700

    add more files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import './App.css';
 import React from 'react';
+import Photo from "./Photo"; 
+
 //import statement
 
 class App extends React.Component {
 
   //fetch API
+  state = {
+    photos: []
+  };  
+
+  componentDidMount () {
+    fetch ("https://jsonplaceholder.typicode.com/photos")
+    .then(res => {
+      if (res.status !== 200){
+        console.log (`This is a error ${res.status}`); 
+      }
+
+      res.json()
+      .then(data => {
+        this.setState({
+          photos:data
+        }); 
+      }); 
+    }); 
+  }
 
   //delete function
+
+  deletePhotos = id => {
+    let currentPhotoList = this.state.photos; 
+    let newPhotoList = currentPhotoList.filter (photo => photo.id !== id);
+    this.setState ({
+      photos:newPhotoList
+    }); 
+  }; 
+
 
   //edit function
   //display none --> blockにするfunction
 
   //================ rendering =============
   render() {
+    const {photos} = this.state; 
     return (
       <>
         <header className="App-header">
           <h1>Weekly Assignment</h1>
         </header>
         {/* Photo.js : Child Component 1 */}
-        <Photo />
+        <Photo photos = {photos} deletePhotos = {this.deletePhotos}/>; 
         {/* Form.js : Child Component 2 */}
-        <Form />
+
       </>
     );
   }
